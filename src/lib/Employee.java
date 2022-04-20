ea48ff211bb4b8a60f78506ddf4a31c105a57870
muhamadjibril <79740456+muhamadjibril@users.noreply.github.com>
2022-04-20 22:50:10 +0700

    Extends class baru, mengubah parameter pada variable
    membuat class baru berisi profile pegawai, meberikan extends class pada class employee, baru dan mengganti parameter pada variable baru

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -5,12 +5,12 @@ import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Employee {
+public class Employee extends Profile {
 
 	private String employeeId;
 	private String firstName;
 	private String lastName;
-	private String idNumber;
+	private int idNumber;
 	private String address;
 	
 	private int yearJoined;
@@ -31,7 +31,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, int idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
---
+++ src/lib/Profile.java
@@ -0,0 +1,42 @@
+package lib;
+
+public class Profile {
+
+  private String employeeId;
+  private String firstName;
+	private String lastName;
+	private int idNumber;
+	private String address;
+}
+
+public String getEmployeeId() {
+  return employeeId;
+}
+public void setEmployeeId(String employeeId) {
+  this.employeeId = employeeId;
+}
+public String getFirstName() {
+  return firstName;
+}
+public void setFirstName(String firstName) {
+  this.firstName = firstName;
+}
+public String getLastName() {
+  return lastName;
+}
+public void setLastName(String lastName) {
+  this.lastName = lastName;
+}
+public int getIdNumber() {
+  return idNumber;
+}
+public void setIdNumber(int idNumber) {
+  this.idNumber = idNumber;
+}
+public String setAddress() {
+  return address;
+}
+public void setAddress(String address) {
+  this.address = address;
+    }
+}
